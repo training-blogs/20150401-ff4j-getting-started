3fc608fab4e7aad94e75843995c1e43542bed77e
Alexis THOMAS <fr.alexisthomas@gmail.com>
2015-04-02 23:58:03 +0200

    Format

--- src/main/java/org/ff4j/sample/App.java
+++ src/main/java/org/ff4j/sample/App.java
@@ -2,12 +2,11 @@ package org.ff4j.sample;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+
 }
